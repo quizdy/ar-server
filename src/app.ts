704b8affd777e73b5d162dbcd5b9796e9b0874b7
quizdy <quizdynet@gmail.com>
2023-01-17 11:17:16 +0900

    20230117cors

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,8 @@ const USERS_PATH = '../users'
 const VENUES_PATH = '../public/venues'
 const IMAGES_PATH = '../public/images'
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 app.use(express.static('images'))
 
 /**
